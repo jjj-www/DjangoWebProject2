5f1d776f473cf797850c18b34d71dafeca853e9e
dipshit <4061942@schoolsnet.act.edu.au>
2026-05-01 11:28:56 +1000

    .

--- DjangoWebProject2/MyApp1/admin.py
+++ DjangoWebProject2/MyApp1/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import teacher
 from .models import unitoutline
 
-# Register your models here..
+# Register your models here...
 admin.site.register(teacher)
 admin.site.register(unitoutline)
